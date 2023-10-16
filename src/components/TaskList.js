c6d1638e102fb2f8290d5c334bbba7b626f95d69
Bilesanmi Ahmad <fbilesanmi@gmail.com>
2023-10-16 09:38:31 +0000

    added new components and props

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,7 +1,9 @@
 import { useState } from 'react'
+import { TaskCard } from './TaskCard'
+import { BoxCard } from './BoxCard'
 
 export const TaskList = (props) => {
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
     const [tasks, setTasks] = useState([
         {id: 5271, name: "Record Lectures", completed: true}, 
         {id: 7825, name: "Edit React Lectures", completed: false}, 
@@ -10,17 +12,17 @@ export const TaskList = (props) => {
 
     const [show, setShow] = useState(true)
 
-    const handleAdd = () => {
-        setCount(count + 1)
-    }
+    // const handleAdd = () => {
+    //     setCount(count + 1)
+    // }
 
-    const handleSub = () => {
-        setCount(count - 1)
-    }
+    // const handleSub = () => {
+    //     setCount(count - 1)
+    // }
 
-    const handleReset = () => {
-        setCount(0)
-    }
+    // const handleReset = () => {
+    //     setCount(0)
+    // }
 
     const handleDelete = (id) => {
         setTasks(tasks.filter(task => id !== task.id))
@@ -32,18 +34,21 @@ export const TaskList = (props) => {
         <ul>
           <button className='trigger' onClick={()=> setShow(!show)}>Toggle</button>
           { show && tasks.map((task) => (
-            <li key={task.id} className={task.completed ? "completed" : "incomplete"}>
-              <span>{task.id} - {task.name}</span>
-              <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
-            </li>
+            <TaskCard key={task.id} task={task} handleDelete={handleDelete} />
           ))}
         </ul>
-        <div className="box">
-            <p>{count}</p>
-            <button onClick={handleAdd} className="add">ADD</button>
-            <button onClick={handleSub} className="sub">SUB</button>
-            <button onClick={handleReset} className="reset">RESET</button>
-        </div>
+        <BoxCard result="success">
+          <p className="title">Lorem ipsum dolor sit amet.</p>
+          <p className="description">Lorem ipsum dolor sit amet consectetur adipis</p>
+        </BoxCard>
+        <BoxCard className="box warning">
+          <p className="title">Lorem ipsum dolor sit amet.</p>
+          <p className="description">Lorem ipsum dolor sit amet consectetur adipis</p>
+        </BoxCard>
+        <BoxCard className="box alert">
+          <p className="title">Lorem ipsum dolor sit amet.</p>
+          <p className="description">Lorem ipsum dolor sit amet consectetur adipis</p>
+        </BoxCard>
     </>  
   )
 }
